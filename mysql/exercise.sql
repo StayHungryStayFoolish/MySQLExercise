bdd0193901bcbfa21fe7e57c3dce83020708678f
StayHungryStayFoolish <bonismo@hotmail.com>
2019-10-28 17:51:47 +0800

    commit

--- mysql/exercise.sql
+++ mysql/exercise.sql
@@ -1,3 +1,6 @@
+# 显示当前 MySQL 版本号
+SELECT version();
+
 SHOW DATABASES;
 
 DROP DATABASE IF EXISTS `mysql_exercise`;
@@ -48,27 +51,62 @@ VALUES (1, '国外食品');
 INSERT INTO category
 VALUES (2, '国内食品');
 INSERT INTO category
-VALUES (3, '国内服装'); #没有对应
+VALUES (3, '国内服装');
+#没有对应
 
-DESC product;
-
-SELECT *
-FROM product;
-
-SELECT version();
-
-UPDATE product
-SET pname = '海底捞'
-WHERE pid = 9;
-
-SELECT DISTINCT pname, price
-FROM product;
-
-SELECT *, count(DISTINCT pname, price, pid)
+# 查询所有字段，其中 pname、price 去重。如果 group by 后边只有 pname 字段，则事实上只对 name 去重，只有 distinct 和 group by  后边字段保持一直，结果才会正确
+SELECT *, count(DISTINCT pname, price)
 FROM product
-GROUP BY pname
+GROUP BY pname, price;
+
+# Distinct 后边的字段都会并入去重条件，当两个字段值同时重复时，只显示一条数据
+SELECT DISTINCT price, pname
+FROM product
 ORDER BY pid;
 
+# Group By 后边的字段都会并入去重条件，当两个字段值同时重复时，只显示一条数据
+# 结果同上
 SELECT pname, price
 FROM product
-GROUP BY product.pname,product.price ;
+GROUP BY product.pname, product.price
+ORDER BY pid;
+
+SELECT *
+FROM product
+WHERE pname = '十三香';
+
+SELECT *
+FROM product
+WHERE price > 60;
+
+SELECT *
+FROM product
+WHERE pname LIKE '%新%';
+
+SELECT *
+FROM product
+WHERE pid IN (2, 5, 8);
+
+SELECT *
+FROM product
+WHERE pid BETWEEN 2 AND 8;
+
+SELECT sum(price)
+FROM product;
+
+SELECT avg(price)
+FROM product;
+
+SELECT count(*)
+FROM product;
+
+# 分组统计每组商品的平均价格，并且平均价格> 60;
+# 注意事项
+# select 语句中的列（非聚合函数列），必须出现在 group by 子句中
+# group by 子句中的列，不一定要出现在 select 语句中
+# 聚合函数只能出现 select 语句中或者 having 语句中，一定不能出现在 where 语句中。
+SELECT cid, avg(price)
+FROM product
+GROUP BY cid
+HAVING avg(price) > 60;
+
